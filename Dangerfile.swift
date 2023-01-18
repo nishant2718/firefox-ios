3356dfdda9605ebe1a641a899928cc0b95ddbd2f
nishant2718 <nishantpatel2718@gmail.com>
2023-01-18 09:05:23 -0500

    Introduce Danger for CI

---
+++ Dangerfile.swift
@@ -0,0 +1,43 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Danger
+import DangerSwiftCoverage
+import Foundation
+
+let danger = Danger()
+
+// MARK: - Execute Reports
+
+changedFiles()
+coverage()
+
+// MARK: - Helpers
+
+func changedFiles() {
+    message("Edited \(danger.git.modifiedFiles.count) files")
+    message("Created \(danger.git.createdFiles.count) files")
+}
+
+func coverage() {
+    guard let xcresult = ProcessInfo.processInfo.environment["BITRISE_XCRESULT_PATH"]?.escapeString() else {
+        fail("Could not get the BITRISE_XCRESULT_PATH to generate code coverage")
+        return
+    }
+
+    Coverage.xcodeBuildCoverage(
+        .xcresultBundle(xcresult),
+        minimumCoverage: 50
+    )
+}
+
+extension String {
+    // Helper function to escape (iOS) in our file name for xcov.
+    func escapeString() -> String {
+        var newString = self.replacingOccurrences(of: "(", with: "\\(")
+        newString = newString.replacingOccurrences(of: ")", with: "\\)")
+        newString = newString.replacingOccurrences(of: " ", with: "\\ ")
+        return newString
+    }
+}
